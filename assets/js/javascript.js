820582dc6b401b2cfccf1d28bdfac4a22314aaba
fatemeh ghani <ghani.fatemeh93@gmail.com>
2025-10-01 16:46:16 +0330

    readonly for mobile2

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,20 +2,20 @@ const btns = document.querySelectorAll(".btn");
 const input = document.getElementById("display-box");
 const equal_btn = document.querySelector("#equal");
 console.log(btns);
-input.focus();
 // بالا نیامدن صفه کلید در موبایل
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
 if (isMobile) {
-  input.setAttribute("readonly", true); // فقط تو موبایل
+  input.addEventListener("focus", (e) => {
+    e.target.blur();
+  });
 } else {
-  input.removeAttribute("readonly");    // تو دسکتاپ عادی
+  input.focus();
 }
 
 // گرفتن ورودی از کیبورد در حالت دسکتاپ
 input.addEventListener("keypress", (e) => {
   const allowed = "1234567890+-*/.Backspace=";
-  console.log(e.key)
+  console.log(e.key);
   if (e.key === "Enter" || e.key === "=") {
     e.preventDefault();
     try {
@@ -24,7 +24,7 @@ input.addEventListener("keypress", (e) => {
       input.value = "error";
     }
     input.focus();
-  }else if (!allowed.includes(e.key)) {
+  } else if (!allowed.includes(e.key)) {
     e.preventDefault();
   }
 });
@@ -32,10 +32,9 @@ input.addEventListener("keypress", (e) => {
 const addInput = (e) => {
   e.preventDefault();
   console.log(input.value);
-  if(e.target.tagName === "I"){
-        e.target.value = e.target.parentElement.value
-
-    }
+  if (e.target.tagName === "I") {
+    e.target.value = e.target.parentElement.value;
+  }
   if (e.target.value === " = ") {
     if (input.value != "") {
       try {
@@ -46,23 +45,23 @@ const addInput = (e) => {
     } else input.value = "";
   } else if (e.target.value === "clear") {
     input.value = "";
-  }else if (e.target.value === "delete"){
+  } else if (e.target.value === "delete") {
     if (input.value.length > 0) {
-    input.value = input.value.slice(0, -1);// تک تک حذف کردن
-
-    }}
-   else {
-    
+      input.value = input.value.slice(0, -1); // تک تک حذف کردن
+    }
+  } else {
     input.value += e.target.value;
   }
+  if (isMobile) {
+  input.addEventListener("focus", (e) => {
+    e.target.blur();
+  });
+} else {
   input.focus();
+}
   e.stopPropagation();
 };
 
 btns.forEach((btn) => {
   btn.addEventListener("click", addInput);
 });
-
-
-
-
